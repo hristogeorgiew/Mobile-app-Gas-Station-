4fa7acff9f9ee250006c1c8a76f775784ae716a9
Hristo <h.georgiew93@gmail.com>
2021-08-18 14:44:36 +0300

    first commit

---
+++ client/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, paddingTop: 120}}>
+        <View style={{height: 100, backgroundColor: 'red'}}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
